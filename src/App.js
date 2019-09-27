f3cf1fbf1c1279f2528cd3e81adc7168af2ef3fa
YellowFreesia <ssj0919@outlook.kr>
2019-09-27 16:10:41 +0900

    Scroll to top 구현

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import TOC from "./TOC";
 import Content from "./Content";
 import {contents} from './contents'
+import ScrollToTop from "./ScrollToTop";
 
 
 function App() {
 
     return (
         <Router>
-            <Switch>
-                <Route exact strict path="/:id"
-                       render={({match, history}) => {
-                           const content = contents.find(item => {
-                               return item.id === Number(match.params.id)
-                           });
-                           if (content)
-                               return <Content history={history} title={content.title} answer={content.answer}/>;
-                           else
-                               return <TOC contents={contents}/>
-                       }}/>
-                <Route render={() => <TOC contents={contents}/>}/>
-            </Switch>
+            <ScrollToTop>
+                <Switch>
+                    <Route exact strict path="/:id"
+                           render={({match, history}) => {
+                               const content = contents.find(item => {
+                                   return item.id === Number(match.params.id)
+                               });
+                               if (content)
+                                   return <Content history={history} title={content.title} answer={content.answer}/>;
+                               else
+                                   return <TOC contents={contents}/>
+                           }}/>
+                    <Route render={() => <TOC contents={contents}/>}/>
+                </Switch>
+            </ScrollToTop>
         </Router>
     )
 }
